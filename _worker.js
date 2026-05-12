1f5c709298955d3b056ebfb97e3d49ac3e596e46
水灵 <132723284+shulng@users.noreply.github.com>
2026-05-13 00:41:44 +0800

    Trae/solo agent n57c8f (#8)

--- _worker.js
+++ _worker.js
@@ -50,7 +50,7 @@ async function 启动传输管道(WS接口, 反代IP) {
 		switch (识别地址类型) {
 			case 1:
 				地址长度 = 4;
-				访问地址 = new Uint8Array(VL数据.slice(地址信息索引, 地址信息索引 + 地址长度)).join('.');
+				访问地址 = Array.from(new Uint8Array(VL数据.slice(地址信息索引, 地址信息索引 + 地址长度))).join('.');
 				break;
 			case 2:
 				地址长度 = new Uint8Array(VL数据.slice(地址信息索引, 地址信息索引 + 1))[0];
